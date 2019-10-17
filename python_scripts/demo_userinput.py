36372bebf99f3cafe54a1e398d44907c3bf8fd04
melmols <54809973+melmols@users.noreply.github.com>
2019-10-17 23:35:11 +0100

    cleaned up code.

--- python_scripts/demo_userinput.py
+++ python_scripts/demo_userinput.py
@@ -1,47 +1,28 @@
-#References:
-## https://github.com/bradymholt/cron-expression-descriptor
-## https://gitlab.com/doctormo/python-crontab
-#License: MIT,
-#Author: Melmols
+#this is a demo example for running cron_parser.py
 
-## 17/10/2019
-## Description:
-#parsers a cron string and expands each fields to show the times at
-#which it will run
 
-#libs
+#importing class cron_parser
 
-#cron translator to human readable
-from cron_descriptor import get_description, ExpressionDescriptor
+from cron_parser import cron_parser_class
 
-class cron_parser:
-    def usage():
-          print 'Example usage: '
-          print 'python %s */15 0 1,15 * 1-5 /usr/bin/find' % sys.argv[0]
-
-          sys.exit(0)
-
-    def cron_translate(cron_slice):
-        '''Translates cron job into human redable'''
-
-        raw_result = get_description(cron_slice)
-        final_result = [x.strip() for x in raw_result.split(',')]
-
-        return final_result
-
-def start_test_input():
+def test_with_input():
     ''' User interaction demo'''
+    cp = cron_parser_class()
 
-    cron_exp = raw_input('Enter Cron Expression: ')
-    job_name = raw_input('Enter job you wish to run: ')
+    cron_exp = input('Enter Cron expression: ')
+    job_name = input('Enter job you wish to run: ')
 
     try:
-        print ('The job: %s will run: ' %job_name)
-        print (cron_translate(cron_exp))
-    except Exception as e: print(e)
+        print ('The job: %s ' %job_name)
+        print ('Will run as follows: ')
+        print (cp.result_table(cron_exp))
+    except Exception as e:
+        print(e)
+        cron_parser_class.usage()
 
 
 
 if __name__ == "__main__":
 
-    start_test_input()
+    test_with_input()
+
